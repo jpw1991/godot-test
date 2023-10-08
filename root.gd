ffd434a709bec53832a7fe97f1c9106a4de13923
Joshua Paul Woods <jpw1991@gmail.com>
2023-10-08 12:44:12 +1100

    working on surface tool gen

--- root.gd
+++ root.gd
@@ -1,5 +1,19 @@
 extends Node
  
+const BOTTOM_LEFT = Vector3(0, 0, -1)
+const BOTTOM_RIGHT = Vector3(1, 0, -1)
+const TOP_LEFT = Vector3(0, 0, 0)
+const TOP_RIGHT = Vector3(1, 0, 0)
+
+const BOTTOM_LEFT_UV = Vector2(0.0, 1.0)
+const BOTTOM_RIGHT_UV = Vector2(1.0, 1.0)
+const TOP_LEFT_UV = Vector2(0.0, 0.0)
+const TOP_RIGHT_UV = Vector2(1.0, 0.0)
+
+var mesh_instance : MeshInstance3D
+var timer = 0.0
+var interval = 1.0
+
 enum TileType {
 	NOTHING,
 	STONE,
@@ -28,7 +42,221 @@ class Tile:
 				return 1
 			_:
 				return 0
- 
+
+
+func generate_surface_tool():
+	var tiles_2d = [
+		[Tile.new(0,0,TileType.WALL),Tile.new(1,0,TileType.WALL),Tile.new(2,0,TileType.WALL)],
+		[Tile.new(0,1,TileType.WALL),Tile.new(1,1,TileType.FLOOR),Tile.new(2,1,TileType.WALL)],
+		[Tile.new(0,2,TileType.WALL),Tile.new(1,2,TileType.WALL),Tile.new(2,2,TileType.WALL)],
+		[Tile.new(0,3,TileType.FLOOR),Tile.new(1,3,TileType.FLOOR),Tile.new(2,3,TileType.FLOOR)],
+		]
+	var result = SurfaceTool.new()
+	result.begin(Mesh.PRIMITIVE_TRIANGLES)
+	result.set_color(Color(1, 0, 0))
+	
+	var index_count = 0
+	for y in range(tiles_2d.size()):
+		for x in range(tiles_2d[y].size()):
+			var tile = tiles_2d[y][x]
+			var face_indices = []
+			var position = Vector3(tile.x, tile.height(), -tile.y)
+			
+#			#result.set_normal(Vector3.UP)
+#			result.set_uv(TOP_LEFT_UV)
+#			result.add_vertex(position + TOP_LEFT)
+#			#result.add_index(++index_count)
+#
+#			#result.set_normal(Vector3.UP)
+#			result.set_uv(TOP_RIGHT_UV)
+#			result.add_vertex(position + TOP_RIGHT)
+#			#result.add_index(++index_count)
+#
+#			#result.set_normal(Vector3.UP)
+#			result.set_uv(BOTTOM_RIGHT_UV)
+#			result.add_vertex(position + BOTTOM_RIGHT)
+#			#result.add_index(++index_count)
+#
+#			#result.set_normal(Vector3.UP)
+#			result.set_uv(BOTTOM_LEFT_UV)
+#			result.add_vertex(position + BOTTOM_LEFT)
+#			#result.add_index(++index_count)
+			
+			# to do: order should be top left, top right, bottom left, bottom right
+			
+			result.add_triangle_fan([position + TOP_LEFT, position + TOP_RIGHT,
+				position + BOTTOM_RIGHT, position + BOTTOM_LEFT],
+				[TOP_LEFT_UV, TOP_RIGHT_UV, BOTTOM_RIGHT_UV, BOTTOM_LEFT_UV],
+				[], [], [Vector3.UP, Vector3.UP, Vector3.UP, Vector3.UP])
+			
+			# create the faces
+			# faces must be created counter-clockwise
+#			faces.append([face_indices[0], face_indices[2], face_indices[1]])
+#			faces.append([face_indices[2], face_indices[3], face_indices[1]])
+			
+			# West wall
+			if x == 0 or tiles_2d[y][x-1].height() < tile.height():
+				face_indices = []
+				
+				var min_height = 0 if x == 0 else tiles_2d[y][x-1].height()
+				var max_height = tile.height()
+				
+				result.add_triangle_fan([Vector3(position.x, min_height, position.z),
+					Vector3(position.x, max_height, position.z),
+					Vector3(position.x, max_height, position.z - 1),
+					Vector3(position.x, min_height, position.z - 1)],
+					[TOP_LEFT_UV, TOP_RIGHT_UV, BOTTOM_RIGHT_UV, BOTTOM_LEFT_UV],
+				[], [], [Vector3.LEFT, Vector3.LEFT, Vector3.LEFT, Vector3.LEFT])
+				
+#				#result.set_normal(Vector3.LEFT)
+#				result.set_uv(TOP_LEFT_UV)
+#				result.add_vertex(Vector3(position.x, min_height, position.z))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.LEFT)
+#				result.set_uv(TOP_RIGHT_UV)
+#				result.add_vertex(Vector3(position.x, max_height, position.z))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.LEFT)
+#				result.set_uv(BOTTOM_RIGHT_UV)
+#				result.add_vertex(Vector3(position.x, max_height, position.z - 1))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.LEFT)
+#				result.set_uv(BOTTOM_LEFT_UV)
+#				result.add_vertex(Vector3(position.x, min_height, position.z - 1))
+#				#result.add_index(++index_count)
+				
+				
+					
+#				faces.append([face_indices[2], face_indices[1], face_indices[0]])
+#				faces.append([face_indices[2], face_indices[3], face_indices[1]])
+				
+			# East wall
+			if x == tiles_2d[y].size() - 1 or tiles_2d[y][x+1].height() < tile.height():
+				face_indices = []
+				
+				var min_height = 0 if x == tiles_2d[y].size() - 1 else tiles_2d[y][x+1].height()
+				var max_height = tile.height()
+				
+				result.add_triangle_fan([Vector3(position.x + 1, min_height, position.z),
+					Vector3(position.x + 1, max_height, position.z),
+					Vector3(position.x + 1, max_height, position.z - 1),
+					Vector3(position.x + 1, min_height, position.z - 1)],
+					[TOP_LEFT_UV, TOP_RIGHT_UV, BOTTOM_RIGHT_UV, BOTTOM_LEFT_UV],
+				[], [], [Vector3.RIGHT, Vector3.RIGHT, Vector3.RIGHT, Vector3.RIGHT])
+				
+#				#result.set_normal(Vector3.RIGHT)
+#				result.set_uv(TOP_LEFT_UV)
+#				result.add_vertex(Vector3(position.x + 1, min_height, position.z))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.RIGHT)
+#				result.set_uv(TOP_RIGHT_UV)
+#				result.add_vertex(Vector3(position.x + 1, max_height, position.z))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.RIGHT)
+#				result.set_uv(BOTTOM_RIGHT_UV)
+#				result.add_vertex(Vector3(position.x + 1, max_height, position.z - 1))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.RIGHT)
+#				result.set_uv(BOTTOM_LEFT_UV)
+#				result.add_vertex(Vector3(position.x + 1, min_height, position.z - 1))
+#				#result.add_index(++index_count)
+				
+				
+				
+				
+				
+#				faces.append([face_indices[0], face_indices[1], face_indices[2]])
+#				faces.append([face_indices[1], face_indices[3], face_indices[2]])
+				
+			# South wall
+			if y == 0 or tiles_2d[y-1][x].height() < tile.height():
+				face_indices = []
+				
+				var min_height = 0 if y == 0 else tiles_2d[y-1][x].height()
+				var max_height = tile.height()
+				
+				result.add_triangle_fan([Vector3(position.x, min_height, position.z),
+					Vector3(position.x, max_height, position.z),
+					Vector3(position.x + 1, max_height, position.z),
+					Vector3(position.x + 1, min_height, position.z)],
+					[TOP_LEFT_UV, TOP_RIGHT_UV, BOTTOM_RIGHT_UV, BOTTOM_LEFT_UV],
+				[], [], [Vector3.BACK, Vector3.BACK, Vector3.BACK, Vector3.BACK])
+					
+#				#result.set_normal(Vector3.BACK)
+#				result.set_uv(TOP_LEFT_UV)
+#				result.add_vertex(Vector3(position.x, min_height, position.z))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.BACK)
+#				result.set_uv(TOP_RIGHT_UV)
+#				result.add_vertex(Vector3(position.x, max_height, position.z))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.BACK)
+#				result.set_uv(BOTTOM_RIGHT_UV)
+#				result.add_vertex(Vector3(position.x + 1, max_height, position.z))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.BACK)
+#				result.set_uv(BOTTOM_LEFT_UV)
+#				result.add_vertex(Vector3(position.x + 1, min_height, position.z))
+#				#result.add_index(++index_count)
+				
+				
+				
+				
+				
+#				faces.append([face_indices[0], face_indices[1], face_indices[2]])
+#				faces.append([face_indices[1], face_indices[3], face_indices[2]])
+				
+			# North wall
+			if y == tiles_2d.size()- 1 or tiles_2d[y+1][x].height() < tile.height():
+				face_indices = []
+				
+				var min_height = 0 if y == tiles_2d.size() - 1 else tiles_2d[y+1][x].height()
+				var max_height = tile.height()
+				
+				result.add_triangle_fan([Vector3(position.x, min_height, position.z - 1),
+					Vector3(position.x, max_height, position.z - 1),
+					Vector3(position.x + 1, max_height, position.z - 1),
+					Vector3(position.x + 1, min_height, position.z - 1)],
+					[TOP_LEFT_UV, TOP_RIGHT_UV, BOTTOM_RIGHT_UV, BOTTOM_LEFT_UV],
+				[], [], [Vector3.FORWARD, Vector3.FORWARD, Vector3.FORWARD, Vector3.FORWARD])
+				
+#				#result.set_normal(Vector3.FORWARD)
+#				result.set_uv(TOP_LEFT_UV)
+#				result.add_vertex(Vector3(position.x, min_height, position.z - 1))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.FORWARD)
+#				result.set_uv(TOP_RIGHT_UV)
+#				result.add_vertex(Vector3(position.x, max_height, position.z - 1))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.FORWARD)
+#				result.set_uv(BOTTOM_RIGHT_UV)
+#				result.add_vertex(Vector3(position.x + 1, max_height, position.z - 1))
+#				#result.add_index(++index_count)
+#
+#				#result.set_normal(Vector3.FORWARD)
+#				result.set_uv(BOTTOM_LEFT_UV)
+#				result.add_vertex(Vector3(position.x + 1, min_height, position.z - 1))
+#				#result.add_index(++index_count)
+				
+				
+				
+				
+				
+#				faces.append([face_indices[2], face_indices[1], face_indices[0]])
+#				faces.append([face_indices[2], face_indices[3], face_indices[1]])
+	
+	return result
  
 func generate_obj():
 	var vertices = []
@@ -217,4 +445,20 @@ func write_obj_file(filename, vertices, normals, uvs, faces):
 	file.close()
  
 func _ready():
-	generate_obj()
+	#generate_obj()
+	var mesh = ArrayMesh.new()
+	
+	var surface_tool = generate_surface_tool()
+	#surface_tool.optimize_indices_for_cache()
+	#surface_tool.generate_normals(false)
+	surface_tool.commit(mesh)
+	
+	mesh_instance = MeshInstance3D.new()
+	mesh_instance.mesh = mesh
+	add_child(mesh_instance)
+
+func _process(delta):
+	timer += delta
+	if timer >= interval:
+		mesh_instance.rotate_y(1)
+		timer = 0.0
